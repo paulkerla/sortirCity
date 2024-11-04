eabccfa5039cc7d6ba14b0ab979ec7458cdaa170
paulkerla <paul.kerlau@gmail.com>
2024-11-04 16:23:27 +0100

    commit 4
    Ajout de tous les entités

---
+++ src/Entity/Sortie.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SortieRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SortieRepository::class)]
+class Sortie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateHeureDebut = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $duree = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date_limite_inscription = null;
+
+    #[ORM\Column]
+    private ?int $nbInscriptionMax = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $infosSortie = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $urlPhoto = null;
+
+    #[ORM\Column]
+    private ?int $lieu_no_lieu = null;
+
+    #[ORM\Column]
+    private ?int $etats_no_etat = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDateHeureDebut(): ?\DateTimeInterface
+    {
+        return $this->dateHeureDebut;
+    }
+
+    public function setDateHeureDebut(\DateTimeInterface $dateHeureDebut): static
+    {
+        $this->dateHeureDebut = $dateHeureDebut;
+
+        return $this;
+    }
+
+    public function getDuree(): ?int
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(?int $duree): static
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+
+    public function getDateLimiteInscription(): ?\DateTimeInterface
+    {
+        return $this->date_limite_inscription;
+    }
+
+    public function setDateLimiteInscription(\DateTimeInterface $date_limite_inscription): static
+    {
+        $this->date_limite_inscription = $date_limite_inscription;
+
+        return $this;
+    }
+
+    public function getNbInscriptionMax(): ?int
+    {
+        return $this->nbInscriptionMax;
+    }
+
+    public function setNbInscriptionMax(int $nbInscriptionMax): static
+    {
+        $this->nbInscriptionMax = $nbInscriptionMax;
+
+        return $this;
+    }
+
+    public function getInfosSortie(): ?string
+    {
+        return $this->infosSortie;
+    }
+
+    public function setInfosSortie(?string $infosSortie): static
+    {
+        $this->infosSortie = $infosSortie;
+
+        return $this;
+    }
+
+    public function getUrlPhoto(): ?string
+    {
+        return $this->urlPhoto;
+    }
+
+    public function setUrlPhoto(?string $urlPhoto): static
+    {
+        $this->urlPhoto = $urlPhoto;
+
+        return $this;
+    }
+
+    public function getLieuNoLieu(): ?int
+    {
+        return $this->lieu_no_lieu;
+    }
+
+    public function setLieuNoLieu(int $lieu_no_lieu): static
+    {
+        $this->lieu_no_lieu = $lieu_no_lieu;
+
+        return $this;
+    }
+
+    public function getEtatsNoEtat(): ?int
+    {
+        return $this->etats_no_etat;
+    }
+
+    public function setEtatsNoEtat(int $etats_no_etat): static
+    {
+        $this->etats_no_etat = $etats_no_etat;
+
+        return $this;
+    }
+}
