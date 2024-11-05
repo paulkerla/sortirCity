e44e48d0525fc9a301b54ef4d748175b8f590e39
MarionQH <marion.huet49@gmail.com>
2024-11-05 16:49:34 +0100

    commit 11
    flash message en cours

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -18,6 +18,13 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
+        if ($this->getUser()) {
+
+            $this->addFlash('success', 'Vous êtes connecté avec succès.');
+
+            return $this->redirectToRoute('/');
+        }
+
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
