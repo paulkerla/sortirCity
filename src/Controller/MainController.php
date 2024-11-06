50a37e3f83a034da362d4c102698eefedbd67880
Blecleach <blecleacheni@gmail.com>
2024-11-06 15:54:23 +0100

    main controller erreur

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,7 +11,7 @@ class MainController extends AbstractController
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
-        return $this->render('main/gestion.html.twig', [
+        return $this->render('main/home.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
