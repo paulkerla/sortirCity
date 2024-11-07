a53f34689aa875555564c67818e00781d31ab96b
Blecleach <blecleacheni@gmail.com>
2024-11-07 11:07:06 +0100

    Homepage

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Meetup;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,24 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $currentDate = new \DateTime();
+
+        $meetups = $entityManager->getRepository(Meetup::class)
+            ->createQueryBuilder('m')
+            ->where('m.registrationlimitdate >= :currentDate')
+            ->setParameter('currentDate', $currentDate)
+            ->orderBy('m.startdatetime', 'ASC') //ASC pour ascendant/croissant
+            ->setMaxResults(5)
+            ->getQuery()
+            ->getResult(); // execute et renvoit sous forme de tableau
+
+
         return $this->render('main/home.html.twig', [
-            'controller_name' => 'MainController',
+            'meetups' => $meetups,
         ]);
     }
+
+
 }
