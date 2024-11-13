fdc9550422706e2ee5e54fb550011cea08eefcc7
MarionQH <marion.huet49@gmail.com>
2024-11-13 16:36:42 +0100

    commit 50
    lever les bras on touche plus

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -7,7 +7,10 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
 #[Route('/admin', name: 'app_admin_')]
+#[IsGranted('ROLE_ADMIN')]
 class AdminController extends AbstractController
 {
     #[Route('/validate/{id}', name: 'validate')]
