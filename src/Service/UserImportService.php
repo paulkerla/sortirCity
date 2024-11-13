4c1387da60e3cd731ffee695dd58575e2b945077
MarionQH <marion.huet49@gmail.com>
2024-11-13 11:34:44 +0100

    commit ter

--- src/Service/UserImportService.php
+++ src/Service/UserImportService.php
@@ -1,1 +1,24 @@
 <?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserImportService
+{
+    private EntityManagerInterface $entityManager;
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->entityManager = $entityManager;
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function importUsers(array $userData): void
+    {
+        // Code pour importer les utilisateurs
+    }
+}
